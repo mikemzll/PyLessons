108f663762c1fc83cba44741c8dfe5a4153df755
mickexamp <mimymy13@yandex.ua>
2018-08-09 15:16:45 +0300

    lesson

--- pyLesson.py
+++ pyLesson.py
@@ -1,2 +1,11 @@
-num = 14
-print( 'I am ' + str(num))
+# num = 14
+# print( 'I am ' + str(num))
+
+# num = 3
+# print( (1 + num) ** 2 ) #степінь
+
+# num = 20
+# print( num // 6) #частин
+
+num = 7
+print( num % 3 )  #ділення по модулю
